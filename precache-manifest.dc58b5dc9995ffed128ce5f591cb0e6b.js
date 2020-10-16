ec251be8693fa9a8f7c0b35ad0b4ebe573fa744d
Booshna-Arthy <booshna012@gmail.com>
2020-10-16 21:02:31 +0530

    Updates

---
+++ precache-manifest.dc58b5dc9995ffed128ce5f591cb0e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b99ebf5ebf824b39d64d1428abd9397",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "0252e1b6b39658bbc32a",
+    "url": "/simple-todo-app/static/css/main.0a7a2c31.chunk.css"
+  },
+  {
+    "revision": "87678083107f468f415e",
+    "url": "/simple-todo-app/static/js/2.77d718ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo-app/static/js/2.77d718ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0252e1b6b39658bbc32a",
+    "url": "/simple-todo-app/static/js/main.19531cae.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
